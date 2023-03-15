1a6d0f8222813fb0225d4641aaf1443a9e0f2731
Carlos Eduardo <Mary65646@gmail.com>
2023-03-15 23:34:48 +0000

    feat: min

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -8,12 +8,13 @@ const routersManager = Router();
 
 routersManager.use((req, res, next) => {
     loggerManager.log({ type: "HTTP", ip: req.headers["X-Real-IP"]?.toString() || "Local", extra: `headers: ${req.rawHeaders.length}` })
+    next();
 });
 
 routersManager.use("/", async (req, res) => {
-
-
-
+    return res.json({
+        server: "OK"
+    });
 });
 
 export default routersManager
